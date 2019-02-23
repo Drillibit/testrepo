47c3b83942c0f044a9f4f82475b6f568e3a69c22
Drillibit <293344asd@gmail.com>
2019-02-23 20:25:07 +0700

    added back button

--- src/components/moviepage/MoviePage.js
+++ src/components/moviepage/MoviePage.js
@@ -29,6 +29,21 @@ const StyledImage = styled.img`
     margin-right: 15px;
 `;
 
+const StyledButton = styled.button`
+    width: 180px;
+    height: 45px;
+    border: none;
+    color: #fff;
+    background-color: deepskyblue;
+    font-size: 20px;
+    margin-left: 5px; 
+    cursor: pointer;
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+    &:hover {
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    }
+`;
+
 export class MoviePage extends Component {
     state = {
         loading: true,
@@ -73,6 +88,7 @@ export class MoviePage extends Component {
                     )
                 })}
                 </StyledInfoWrapper>
+                <StyledButton onClick={() => this.props.history.goBack()}>Back</StyledButton>  
             </StyledPageWrapper>
         );
     }
